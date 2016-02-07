f265050dea4a020baa93fd4aae5d98bda19a5a83
webexninja <shivanktiware@gmail.com>
2016-02-07 22:55:48 +0530

    Add tests for word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,4 @@
 class Word < ActiveRecord::Base
+	validates :title, presence: true, length: {maximum: 15}, :uniqueness=> true
+	validates :meaning, presence: true, length: {maximum: 15}
 end
--- test/models/word_test.rb
+++ test/models/word_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class WordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@word = Word.new
+		@word_one = words(:humour)
+	end
+	test "should not save words without name and meaning" do
+		assert_not @word.save
+	end
+	test "should not save words with long title and name" do
+		@word.title = "a"*16
+		@word.meaning = "a" * 16
+		assert_not @word.save
+	end
+	test "should not allow duplicate title" do
+		@word = @word_one.dup
+		assert_not @word.save
+	end
 end
+
